31c8b7e8adeadc732eb0d74aef959008cc7d78cb
Yiduu <147281138+Yiduu@users.noreply.github.com>
2026-05-11 14:55:23 +0300

    Remove comment on ForeignKey use_alter option

--- models.py
+++ models.py
@@ -38,7 +38,6 @@ class User(db.Model):
 class Message(db.Model):
     __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
-    # Fixed: use_alter=True to avoid circular FK dependency
     sender_id = db.Column(db.Integer, db.ForeignKey('users.id', use_alter=True, name='fk_messages_sender'), nullable=False)
     recipient_id = db.Column(db.Integer, db.ForeignKey('users.id', use_alter=True, name='fk_messages_recipient'), nullable=False)
     content = db.Column(db.Text, nullable=False)
